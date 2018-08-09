17868b5e735eab65c470245f12fd3440ba4c97ca
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-08-09 23:34:35 +0100

    more

--- lib/config.go
+++ lib/config.go
@@ -1,18 +1,11 @@
 package lib
 
 import (
-	"os"
-
-	"github.com/Sirupsen/logrus"
-	"github.com/davecgh/go-spew/spew"
+	
 )
 
 type Config struct{}
 
-func (this *Config) Read(configFileName string) {
-	file, err := os.Open(configFileName)
-	if err != nil {
-		logrus.Fatal(err.Error())
-	}
-	spew.Dump(file)
+func (conf *Config) Read(configFileName string) {
+	
 }
--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"strconv"
+
 	"./lib"
 	"github.com/Sirupsen/logrus"
+	"github.com/tsuru/config"
 )
 
 func main() {
 	log := logrus.New()
 	log.Info("Process started!")
 	// read config file
-	config := lib.Config{}
-	config.Read("./config.yml")
+	config.ReadConfigFile("./config.yml")
+	portStr, _ := config.GetString("site_port")
+	port, _ := strconv.Atoi(portStr)
 
 	httpserver := lib.HTTPServer{}
-	httpserver.Serve(8282)
+	httpserver.Serve(port)
 	// serve http on separate thread
 	// init scrapers (one per source)
 	// save data
