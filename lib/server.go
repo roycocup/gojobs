d52a21abade3ae04253c88b32165b718b7564da2
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-08-13 22:52:57 +0100

    Config working

--- lib/server.go
+++ lib/server.go
@@ -11,6 +11,7 @@ import (
 type HTTPServer struct{}
 
 func (this *HTTPServer) Serve(port int) {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"./lib"
 	"github.com/Sirupsen/logrus"
-	"github.com/davecgh/go-spew/spew"
-	config "github.com/micro/go-config"
+	"github.com/micro/go-config"
 	"github.com/micro/go-config/source/file"
 )
 
@@ -17,15 +14,14 @@ func main() {
 	eChecker := lib.Error{}
 
 	// read config file
-	err := config.Load(file.NewSource(file.WithPath("./config.yml")))
+	conf := config.NewConfig()
+	err := conf.Load(file.NewSource(file.WithPath("./config.yml")))
 	eChecker.Check(err)
 
-	port, err := cfg.Get("site.port")
-	spew.Dump(port, err)
-	os.Exit(100)
+	port := config.Get("site", "port").Int(9000)
 
-	// httpserver := lib.HTTPServer{}
-	// httpserver.Serve(port)
+	httpserver := lib.HTTPServer{}
+	httpserver.Serve(port)
 	// serve http on separate thread
 	// init scrapers (one per source)
 	// save data
